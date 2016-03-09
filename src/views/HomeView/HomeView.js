12c982cc7e4e49f972c82eeec88b4e9ce1b5410d
kotazi <kotazi@kotazi.com>
2016-03-09 15:13:13 +0900

    First commit

---
+++ src/views/HomeView/HomeView.js
@@ -0,0 +1,14 @@
+/* @flow */
+import React from 'react'
+
+export class HomeView extends React.Component {
+  render () {
+    return (
+      <div>
+        <h1>Ec Content Hub</h1>
+      </div>
+    )
+  }
+}
+
+export default HomeView
